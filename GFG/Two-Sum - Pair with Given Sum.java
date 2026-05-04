6dcd34daed7f45f8fb839ae776dc95394f21cdc9
Sanjay N <sanjayn29.aiml@gmail.com>
2026-05-04 23:01:57 +0530

    gfg - two sum

---
+++ GFG/Two-Sum - Pair with Given Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        // code here
+        HashMap <Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            int balance = target - arr[i];
+            if(map.containsKey(balance)){
+                return true;
+            }
+            map.put(arr[i],i);
+        }
+        return false;
+    }
+}
