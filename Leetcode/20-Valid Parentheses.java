90597a299f36453283ef95ef8855b299d78316f4
Sanjay N <sanjayn29.aiml@gmail.com>
2026-05-17 23:24:38 +0530

    valid parentheses

---
+++ Leetcode/20-Valid Parentheses.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char i : s.toCharArray()){
+            if (i == '[' || i == '{' || i == '('){
+                stack.push(i);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.peek();
+                if( (i == '}' && top == '{') || (i == ']' && top =='[' ) || ( i == ')' && top == '(' ) ){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
