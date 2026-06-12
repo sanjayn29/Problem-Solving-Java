cdb9dc0951697d3cbc6634eb38bacd5787abd477
Sanjay N <sanjayn29.aiml@gmail.com>
2026-06-12 22:58:00 +0530

    neeum poi podu

---
+++ GFG/Set Matrix Zeros.java
@@ -0,0 +1,61 @@
+class Solution {
+    public void setMatrixZeroes(int[][] matrix) {
+        // code here
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean fr = false;
+        boolean fc = false;
+
+        for(int j=0;j<n;j++){
+            if(matrix[0][j] == 0){
+                fr = true;
+                break;
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            if(matrix[i][0] == 0){
+                fc = true;
+                break;
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            if(matrix[i][0] == 0){
+                for(int j=1;j<n;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        for(int j=1;j<n;j++){
+            if(matrix[0][j] == 0){
+                for(int i=1;i<m;i++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(fr){
+            for(int j=0;j<n;j++){
+                matrix[0][j]=0;
+            }
+        }
+
+        if(fc){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+}
