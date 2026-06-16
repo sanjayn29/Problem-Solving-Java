c554fb2455e3ab6dafaf9b9a60b01a042ac40726
Sanjay N <sanjayn29.aiml@gmail.com>
2026-06-16 22:35:43 +0530

    trapping rain water

---
+++ GFG/Trapping Rain Water.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxWater(int height[]) {
+        // code here
+        int left = 0;
+        int right = height.length-1;
+        int leftmax = 0;
+        int rightmax = 0;
+        int water = 0;
+
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= leftmax){
+                    leftmax = height[left];
+                }else{
+                    water += leftmax-height[left];
+                }
+                left++;
+            }else{
+                if(height[right] >= rightmax){
+                    rightmax = height[right];
+                }else{
+                    water += rightmax-height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
+}
