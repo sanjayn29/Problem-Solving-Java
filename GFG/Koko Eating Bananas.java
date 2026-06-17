f5709f9bfdc536445d3149e3c9a88a6b36c30ec7
Sanjay N <sanjayn29.aiml@gmail.com>
2026-06-17 15:51:19 +0530

    koko eating bananas

---
+++ GFG/Koko Eating Bananas.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int kokoEat(int[] piles, int h) {
+        int low = 1;
+        int high = 0;
+        for(int i : piles){
+            high = Math.max(i,high);
+        }
+        int ans = high;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            long hour = 0;
+            for(int i:piles){
+                hour += (i+mid-1)/mid;
+            }
+            if(hour <= h){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
