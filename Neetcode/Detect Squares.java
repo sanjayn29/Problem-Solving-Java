3bcc77faccac588debc07559650c7374c174bb30
Sanjay N <sanjayn29.aiml@gmail.com>
2026-06-24 14:53:56 +0530

    Detect Squares

---
+++ Neetcode/Detect Squares.java
@@ -0,0 +1,34 @@
+class CountSquares {
+
+    HashMap<String, Integer> map;
+    public CountSquares() {
+        map = new HashMap<>();
+    }
+    
+    public void add(int[] point) {
+        String key = point[0] + "," + point[1];
+        map.put(key, map.getOrDefault(key,0)+1);
+    }
+    
+    public int count(int[] point) {
+        int x = point[0];
+        int y = point[1];
+        int result = 0;
+
+        for(String key : map.keySet()){
+            String[] parts = key.split(",");
+            int px = Integer.valueOf(parts[0]);
+            int py = Integer.valueOf(parts[1]);
+
+            if (Math.abs(px-x) != Math.abs(py-y) || px == x){
+                continue;
+            }
+
+            String p1 = px+","+y;
+            String p2 = x+","+py;
+
+            result += map.get(key)*map.getOrDefault(p1,0)*map.getOrDefault(p2,0);
+        }
+        return result;
+    }
+}
