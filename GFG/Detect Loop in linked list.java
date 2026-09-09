a439d10d7af54b6f128a37ad09a112cfcb7a0eda
Sanjay N <sanjayn29.aiml@gmail.com>
2026-09-09 07:56:15 +0530

    cycle occured ?

---
+++ GFG/Detect Loop in linked list.java
@@ -0,0 +1,31 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public boolean detectLoop(Node head) {
+        // code here
+        if(head == null || head.next == null){
+            return false;
+        }
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
